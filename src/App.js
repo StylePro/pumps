f6994ce85cf34a08dfcb55d5e1560a97d2364d5b
oleg-imac <oleg.sergeev@gmail.com>
2023-10-21 21:59:22 +0300

    new component select pump

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import './index.css'
+import {useDispatch, useSelector} from "react-redux";
+import {newPump} from "./store/dataSlice";
 
 function App() {
+    const pumps = useSelector(state => state.propertiesPumps)
+    const dispatch = useDispatch();
+    function addPump(e) {
+        dispatch(newPump(e.target.value))
+    }
   return (
     <div className="App">
         <div>
-
+            <label> Тип насоса:
+                <select value={pumps.pump || ''} onChange={(e)=> addPump(e)}>
+                    <option value='' disabled hidden>-выберите насос-</option>
+                    {pumps.property.map(pump => <option key={pump.id} value={pump.value}>{pump.value}</option>)}
+                </select>
+            </label>
         </div>
     </div>
   );
--- src/store/dataSlice.js
+++ src/store/dataSlice.js
@@ -1,7 +1,14 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {v4 as uuidv4} from 'uuid';
 
 const initialState = {
-    property: ''
+    pump: '',
+    property: [
+        {id: uuidv4(), value: 'ГХ'},
+        {id: uuidv4(), value: 'ГХМ'},
+        {id: uuidv4(), value: 'ГХС'},
+        {id: uuidv4(), value: 'ГХИ'},
+    ]
 }
 
 
@@ -9,10 +16,12 @@ const propertiesSlice = createSlice({
         name: 'propertiesPumps',
         initialState,
         reducers: {
-
+            newPump(state, action) {
+                state.pump = action.payload
+            }
         }
     }
 )
 
-export const {} = propertiesSlice.actions;
+export const {newPump} = propertiesSlice.actions;
 export default propertiesSlice.reducer;
